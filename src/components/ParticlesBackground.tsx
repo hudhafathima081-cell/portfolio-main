51573841efef2a515a903d898ea3b766eec7aae5
hudha081 <hudhafathima081@gmail.com>
2026-04-25 15:05:53 +0530

    background found error

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -1,12 +1,12 @@
 const ParticlesBackground = () => {
   return (
-    <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
+    <div className="fixed inset-0 z-50 pointer-events-none">
 
-      <span className="absolute w-6 h-6 bg-white/30 rounded-full left-[10%] bottom-[-50px] animate-bubble"></span>
-      <span className="absolute w-4 h-4 bg-white/20 rounded-full left-[30%] bottom-[-60px] animate-bubble"></span>
-      <span className="absolute w-8 h-8 bg-white/25 rounded-full left-[60%] bottom-[-70px] animate-bubble"></span>
-      <span className="absolute w-3 h-3 bg-white/20 rounded-full left-[80%] bottom-[-80px] animate-bubble"></span>
-      <span className="absolute w-5 h-5 bg-white/30 rounded-full left-[50%] bottom-[-90px] animate-bubble"></span>
+      <span className="absolute w-6 h-6 bg-white rounded-full left-[10%] bottom-0 animate-bubble"></span>
+      <span className="absolute w-4 h-4 bg-white rounded-full left-[30%] bottom-0 animate-bubble"></span>
+      <span className="absolute w-8 h-8 bg-white rounded-full left-[60%] bottom-0 animate-bubble"></span>
+      <span className="absolute w-3 h-3 bg-white rounded-full left-[80%] bottom-0 animate-bubble"></span>
+      <span className="absolute w-5 h-5 bg-white rounded-full left-[50%] bottom-0 animate-bubble"></span>
 
     </div>
   );
