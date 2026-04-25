1af0d5d48b0261a4879f6d905df04c4fa81249ce
hudha081 <hudhafathima081@gmail.com>
2026-04-25 17:04:17 +0530

    correctng

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -1,36 +1,16 @@
-import { useMemo } from "react";
-
 const ParticlesBackground = () => {
-
-  const bubbles = useMemo(() => {
-    return Array.from({ length: 25 }).map((_, i) => ({
-      id: i,
-      size: Math.random() * 6 + 4, // 4px - 10px
-      left: Math.random() * 100, // random horizontal position
-      duration: Math.random() * 10 + 8, // 8s - 18s
-      delay: Math.random() * 5, // delay start
-      opacity: Math.random() * 0.5 + 0.3,
-    }));
-  }, []);
-
   return (
     <div className="fixed inset-0 -z-10 pointer-events-none overflow-hidden">
 
-      {bubbles.map((b) => (
-        <span
-          key={b.id}
-          className="absolute bg-white rounded-full"
-          style={{
-            width: `${b.size}px`,
-            height: `${b.size}px`,
-            left: `${b.left}%`,
-            bottom: "0px",
-            opacity: b.opacity,
-            animation: `bubble ${b.duration}s linear infinite`,
-            animationDelay: `${b.delay}s`,
-          }}
-        />
-      ))}
+      <span className="absolute w-10 h-10 bg-white rounded-full left-[10%] bottom-0 animate-bubble shadow-[0_0_25px_white]"></span>
+
+      <span className="absolute w-8 h-8 bg-white rounded-full left-[30%] bottom-0 animate-bubble shadow-[0_0_20px_white] [animation-duration:8s]"></span>
+
+      <span className="absolute w-12 h-12 bg-white rounded-full left-[60%] bottom-0 animate-bubble shadow-[0_0_30px_white] [animation-duration:12s]"></span>
+
+      <span className="absolute w-6 h-6 bg-white rounded-full left-[80%] bottom-0 animate-bubble shadow-[0_0_15px_white] [animation-duration:6s]"></span>
+
+      <span className="absolute w-9 h-9 bg-white rounded-full left-[50%] bottom-0 animate-bubble shadow-[0_0_25px_white] [animation-duration:10s]"></span>
 
     </div>
   );
