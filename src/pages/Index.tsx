cbb5465ea3d2993130d5bf9d9a6f23ab6d462682
hudha081 <hudhafathima081@gmail.com>
2026-04-25 16:47:37 +0530

    changed index  section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
 const Index = () => (
-  <div className="min-h-screen bg-background">
+  <div className="min-h-screen bg-gradient-to-br from-[#020617] via-[#0f172a] to-[#1e293b]">
     <Navbar />
     <HeroSection />
     <AboutSection />
