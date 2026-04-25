8877ba8cf6556cecfc5cd14c0b5860f3ea7db8f8
hudha081 <hudhafathima081@gmail.com>
2026-04-25 16:34:08 +0530

    changed edu section

--- src/components/EducationSection.tsx
+++ src/components/EducationSection.tsx
@@ -9,7 +9,12 @@ const education = [
 ];
 
 const EducationSection = () => (
-  <section id="education" className="py-24">
+  <section
+  id="education"
+  className="min-h-screen py-20 
+  bg-gradient-to-br from-[#020617] via-[#0f172a] to-[#1e293b] 
+  transition-all duration-700"
+>
     <div className="container mx-auto px-6">
       <SectionHeading title="Education" subtitle="My academic journey" />
 
