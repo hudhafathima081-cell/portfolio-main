ab9031276ab91be848e25ca0906cfb722d535149
hudha081 <hudhafathima081@gmail.com>
2026-06-09 14:50:34 +0530

    making premium

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // ✅ ADD THIS
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import Lenis from "@studio-freight/lenis";
+
+// LENIS SMOOTH SCROLL
+const lenis = new Lenis({
+  duration: 1.5,
+  smoothWheel: true,
+});
+
+// ANIMATION FRAME
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
